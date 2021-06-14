6ae2041c0b08f986459661f794a023feef1d5840
NamePoet <y1813880702@163.com>
2021-06-14 17:50:26 +0800

    Bonjour java

---
+++ testjava/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String args[]){
+        System.out.print("Hello");
+        System.out.println(" World! ");
+        System.out.println("Hello World!");
+        System.out.println("你好，世界！");
+    }
+}
